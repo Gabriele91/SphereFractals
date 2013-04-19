05cacfe0eff75587ef363166f396c81940dc4c3c
Gabriele Di Bari <dbgabri@gmail.com>
2013-04-19 16:37:30 +0200

    Added sphere surface!

--- main.cpp
+++ main.cpp
@@ -6,65 +6,139 @@
 
 using namespace Sphere;
 
-class SolidSphere
-{
+class TheSphere {
+
+	//sphere radius
+	float radius;
+	//division sphere
+	int rings;
+	int sections;
+	//value for normalize a section and a ring point
+	float nrmRings;
+	float nrmSections;
+
+public:
+	
+	TheSphere(float radius,
+			   int rings,
+			   int sections)
+			:radius(radius)
+			,rings(rings)
+			,sections(sections)
+			,nrmRings(1./(float)(rings-1))
+			,nrmSections(1./(float)(sections-1))
+	{}
+
+	DFORCEINLINE float getSection(int section){
+		return section*nrmSections;
+	}	
+	DFORCEINLINE float getRing(int ring){
+		return ring*nrmRings;
+	}
+	DFORCEINLINE int getSections(){
+		return sections;
+	}	
+	DFORCEINLINE int getRings(){
+		return rings;
+	}
+	DFORCEINLINE int getRadius(){
+		return radius;
+	}
+
+};
+
+class SphereSruface{
+	
 protected:
-    std::vector<GLfloat> vertices;
-    std::vector<GLfloat> normals;
-    std::vector<GLfloat> texcoords;
+	
+	struct GLVertex{
+		Vec3 vertex;
+		Vec3 color;
+	};
+
+	std::vector<GLVertex> vertices;
     std::vector<GLushort> indices;
+	TheSphere *ptrSphere;
 
 public:
 
-    SolidSphere(float radius, unsigned int rings, unsigned int sectors)
-    {
-        float const R = 1./(float)(rings-1);
-        float const S = 1./(float)(sectors-1);
-        int r, s;
+	SphereSruface(TheSphere* argSphere,
+				  //start and end rings
+				  int sRings,
+				  int eRings,
+				  //start and end sections
+				  int sSection,
+				  int eSection)
+				  :ptrSphere(argSphere)
+	{
 
-        vertices.resize(rings * sectors * 3);
-        normals.resize(rings * sectors * 3);
-        texcoords.resize(rings * sectors * 2);
-        std::vector<GLfloat>::iterator v = vertices.begin();
-        std::vector<GLfloat>::iterator n = normals.begin();
-        std::vector<GLfloat>::iterator t = texcoords.begin();
-        for(r = 0; r < rings; r++) for(s = 0; s < sectors; s++) {
-                float const y = sin( -  Math::PI/2. + Math::PI * r * R );
-                float const x = cos(  2*Math::PI * s * S) * sin( Math::PI * r * R );
-                float const z = sin(  2*Math::PI * s * S) * sin( Math::PI * r * R );
-
-                *t++ = s*S;
-                *t++ = r*R;
-
-                *v++ = x * radius;
-                *v++ = y * radius;
-                *v++ = z * radius;
-
-                *n++ = x;
-                *n++ = y;
-                *n++ = z;
-        }
-
-        indices.resize(rings * sectors * 4);
-        std::vector<GLushort>::iterator i = indices.begin();
-        for(r = 0; r < rings-1; r++) for(s = 0; s < sectors-1; s++) {
+		for(int r = sRings; r<eRings; ++r){
+			for(int s= sSection; s<eSection; ++s){
+				GLVertex glvertex;
+				spherePoint(glvertex.vertex,r,s);
+				glvertex.color.x=ptrSphere->getRing(r);
+				glvertex.color.y=ptrSphere->getSection(s);
+				glvertex.color.x=ptrSphere->getRing(eRings)*ptrSphere->getSection(eSection);
+				vertices.push_back(glvertex);
+			}
+		}
+		//range
+		int rings=eRings-sRings;
+		int sectors=eSection-sSection;
+		/*		
+        *i++ = r * sectors + s;
+        *i++ = r * sectors + (s+1);
+        *i++ = (r+1) * sectors + (s+1);
+        *i++ = (r+1) * sectors + s;
+		*/
+		/*      1   2
+				*---*--------
+				|   |
+		        *---*--------
+				4   3
+		*/
+		indices.resize(rings * sectors * 6);
+        auto i = indices.begin();
+        for(int r = 0; r < rings-1; r++) {
+			for(int s = 0; s < sectors-1; s++) {
+				//1 2 3
                 *i++ = r * sectors + s;
                 *i++ = r * sectors + (s+1);
                 *i++ = (r+1) * sectors + (s+1);
+				//1 3 4
+                *i++ = r * sectors + s;
+                *i++ = (r+1) * sectors + (s+1);
                 *i++ = (r+1) * sectors + s;
-        }
-    }
-    void draw(bool lines=false){
+			}
+		}
+	}
+	
+	void spherePoint(Vec3& point,int ring, int section){
+
+		float const y = sin( -  Math::PI/2. + Math::PI * ptrSphere->getRing(ring));
+
+        float const x = cos(  2*Math::PI * ptrSphere->getSection(section)) *
+						sin( Math::PI * ptrSphere->getRing(ring) );
+
+        float const z = sin(  2*Math::PI * ptrSphere->getSection(section)) * 
+			            sin( Math::PI * ptrSphere->getRing(ring) );
+
+		point.x=x*ptrSphere->getRadius();
+		point.y=y*ptrSphere->getRadius();
+		point.z=z*ptrSphere->getRadius();
+	}
+
+	void draw(bool lines=false){
 		//
 		if(lines)
 			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		//
-        glVertexPointer(3, GL_FLOAT, 0, &vertices[0]);
-        glNormalPointer(GL_FLOAT, 0, &normals[0]);
-        glTexCoordPointer(2, GL_FLOAT, 0, &texcoords[0]);
-        glDrawElements(GL_QUADS, indices.size(), GL_UNSIGNED_SHORT, &indices[0]);
+        glVertexPointer(3,  GL_FLOAT,sizeof(GLVertex),&vertices[0]);
+		glColorPointer (3,  GL_FLOAT,sizeof(GLVertex),((char*)&vertices[0])+sizeof(Vec3));
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_SHORT, &indices[0]);
         glPopMatrix();
     }
+
 };
 
 class SphereInstance : public MainInstance,
@@ -87,12 +161,33 @@ class SphereInstance : public MainInstance,
 
 public:
 
-	SolidSphere sphere;
+	TheSphere sphere;
+	SphereSruface 
+		sphereSurface1,
+		sphereSurface2,
+		sphereSurface3,
+		sphereSurface4,
+		sphereSurface5,
+		sphereSurface6,
+		sphereSurface7,
+		sphereSurface8;
 	Camera camera;
 
 	SphereInstance()
-		:MainInstance("Sphere",1280,800,32,60,true)
-		,sphere(1, 12, 24){}
+		:MainInstance("Sphere",1280,800,32,60,false)
+		,sphere(1,320,320)
+
+		//up
+		,sphereSurface1(&sphere,0,160,0,80)
+		,sphereSurface2(&sphere,0,160,80,160)
+		,sphereSurface3(&sphere,0,160,160,240)
+		,sphereSurface4(&sphere,0,160,240,320)
+		//down
+		,sphereSurface5(&sphere,160,320,0,80)
+		,sphereSurface6(&sphere,160,320,80,160)
+		,sphereSurface7(&sphere,160,320,160,240)
+		,sphereSurface8(&sphere,160,320,240,320)	
+	{}
 
 
 	void start(){
@@ -106,7 +201,7 @@ public:
 						 Application::instance()->getScreen()->getHeight());
 		//enable culling
 		glEnable( GL_CULL_FACE );
-		glCullFace( GL_BACK );
+		glCullFace( GL_FRONT );
 		//set projection matrix
 		projectionAngle=45.0f;
 		setProjection(projectionAngle,0.1f,1000.0f);
@@ -115,8 +210,7 @@ public:
 		glLoadIdentity();
 		//enable state
         glEnableClientState(GL_VERTEX_ARRAY);
-        glEnableClientState(GL_NORMAL_ARRAY);
-        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+        glEnableClientState(GL_COLOR_ARRAY);
 		/////////////////////////////////////////////
 		//camera position
 		camera.setPosition(Vec3(0,0,-20));
@@ -131,7 +225,14 @@ public:
 		glMatrixMode(GL_MODELVIEW);
 		glLoadMatrixf(camera.getGlobalMatrix());
 		//draw
-		sphere.draw(true);
+		sphereSurface1.draw(false);
+		sphereSurface2.draw(false);
+		sphereSurface3.draw(false);
+		sphereSurface4.draw(false);
+		sphereSurface5.draw(false);
+		sphereSurface6.draw(false);
+		sphereSurface7.draw(false);
+		sphereSurface8.draw(false);
 	}
 	void end(){
 	}
@@ -157,11 +258,11 @@ public:
 		//
 		Quaternion quadX,quadY;
 		quadX.setFromEulero(0,
-						    norm.x*Math::PI,
+						    norm.x*Math::PI*2,
 							0);
 		quadY.setFromEulero(0,
 						    0,
-						    -norm.y*Math::PI);
+						    -norm.y*Math::PI*2);
 		//
 		camera.setRotation(quadX.mul(quadY));
 	}
